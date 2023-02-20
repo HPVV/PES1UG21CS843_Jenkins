eb0c8ca8c3931615846f44b6338474488ba03acb
HPVV <104684835+HPVV@users.noreply.github.com>
2023-02-20 10:24:44 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,9 +3,7 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run\n";
-  cout << "PES1UG21CS843_VISHAL S SALANKE\n";
+  cout << "PES1UG21CS843_VISHAL_S_SALANKE\n";
+  cout << "Changes made";
   return 0;
 }
